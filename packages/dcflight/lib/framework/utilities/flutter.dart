80231082111f2edd1b860ed4d53132cf564380a0
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-04-30 14:13:42 +0200

    update

---
+++ packages/dcflight/lib/framework/utilities/flutter.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+export 'package:flutter/material.dart' hide runApp;
+export 'package:flutter/foundation.dart';
+export 'package:flutter/services.dart';
+
+void runWidgetApp(Widget widget)=>runApp(widget);
