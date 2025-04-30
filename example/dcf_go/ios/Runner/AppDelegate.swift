7903767add3e1a4a43cddc0e9ba2f034eddbfb8b
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-05-01 01:55:34 +0200

    Update AppDelegate.swift

--- example/dcf_go/ios/Runner/AppDelegate.swift
+++ example/dcf_go/ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
