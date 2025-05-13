17da832409544e07702d63c817220684805f2588
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-05-13 13:43:29 +0200

    Update app.dart

--- packages/debug_test/dcf_go/lib/app/app.dart
+++ packages/debug_test/dcf_go/lib/app/app.dart
@@ -1,12 +1,46 @@
-import 'package:dcf_router/dcf_router.dart';
+import 'package:dcf_primitives/dcf_primitives.dart';
 import 'package:dcflight/dcflight.dart';
 
 class DCFGo extends StatefulComponent {
   @override
   VDomNode render() {
-    return view(
-      style: StyleSheet(backgroundColor: Colors.amber),
-      layout: LayoutProps(flex: 1),
+    return scrollView(
+      scrollViewProps: ScrollViewProps(
+        showsIndicator: true,
+        clipsToBounds: true,
+      ),
+      style: StyleSheet(),
+      layout: LayoutProps(
+        flex: 1,
+        padding: 8,
+        paddingVertical: ScreenUtilities.instance.statusBarHeight,
+        alignContent: YogaAlign.center,
+        alignItems: YogaAlign.center
+      ),
+      children: [
+        image(imageProps: ImageProps(source: 'assets/logo_bg.png'),layout: LayoutProps(
+          width: 150,
+          height: 150,
+          padding: 8,
+          marginBottom: 16,
+        ),style: StyleSheet(
+          borderRadius:50,
+        )),
+        text(
+          content: 'Build crossplatform apps Native apps in DCFGO',
+          textProps: TextProps(fontSize: 24, color: Colors.black,numberOfLines: 3),
+        ),
+        button(
+          onPress: () {
+            print('Button clicked!');
+          },
+        layout: LayoutProps(width: 200),
+          buttonProps: ButtonProps(title: 'Click Me'),
+        ),
+      ],
     );
   }
 }
+
+
+
