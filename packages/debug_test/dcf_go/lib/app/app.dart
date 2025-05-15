71cba539668e4f5473950b4787db068581d21c74
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-05-16 01:19:30 +0200

    Update app.dart

--- packages/debug_test/dcf_go/lib/app/app.dart
+++ packages/debug_test/dcf_go/lib/app/app.dart
@@ -1,9 +1,12 @@
 import 'package:dcf_primitives/dcf_primitives.dart';
 import 'package:dcflight/dcflight.dart';
 
-class DCFGo extends StatefulComponent{
+class DCFGo extends StatefulComponent {
   @override
   VDomNode render() {
-  return DCFView(layout: LayoutProps(flex: 1)).render();
+    return DCFView(
+      layout: LayoutProps(flex: 1),
+      style: StyleSheet(backgroundColor: Colors.amber),
+    ).render();
   }
 }
